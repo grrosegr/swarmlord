8c3c1e2cd119132481be3cea95d8b3f0ae0eb329
Gregory Rose <greg@ncroses.com>
2014-11-07 16:25:06 -0500

    update prefabs

--- Swarmlord/Assets/Scripts/SwarmMemberController.cs
+++ Swarmlord/Assets/Scripts/SwarmMemberController.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Swarm_member_controller : MonoBehaviour {
+public class SwarmMemberController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
