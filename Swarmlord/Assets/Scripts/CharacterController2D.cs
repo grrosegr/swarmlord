ce2e087722182d39d0e078a6de4de046dad8f4c0
Gregory Rose <greg@ncroses.com>
2014-11-14 16:17:44 -0500

    char controller basic

--- Swarmlord/Assets/Scripts/CharacterController2D.cs
+++ Swarmlord/Assets/Scripts/CharacterController2D.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class CharacterController2D : MonoBehaviour {
 
+	public float Speed = 0.5f;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +12,11 @@ public class CharacterController2D : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		Vector2 pos = transform.position;
+		pos.x += Input.GetAxis("Horizontal") * Speed;
+		pos.y += Input.GetAxis("Vertical") * Speed;
+		
+		transform.position = pos;
 	
 	}
 }
