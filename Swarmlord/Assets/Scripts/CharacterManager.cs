8b678766e9093682d2f3cab22c769c0bd344b3dd
Gregory Rose <greg@ncroses.com>
2014-11-14 18:34:03 -0500

    add next level detect

---
+++ Swarmlord/Assets/Scripts/CharacterManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CharacterManager : MonoBehaviour {
+
+	GameObject[] players;
+	GameObject currentPlayer;
+
+	// Use this for initialization
+	void Start () {
+		players = GameObject.FindGameObjectsWithTag("Player");
+		
+		currentPlayer = players[0];
+		currentPlayer.SendMessage("SetIsControlled", true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for (int i = 1; i <= players.Length; i++) {
+			if (Input.GetKeyDown(i.ToString())) {
+				GameObject oldPlayer = currentPlayer;
+				currentPlayer = players[i - 1];
+				if (oldPlayer != currentPlayer) {
+					oldPlayer.SendMessage("SetIsControlled", false);
+					currentPlayer.SendMessage("SetIsControlled", true);
+				}
+				break;
+			}
+		}
+	
+	}
+}
