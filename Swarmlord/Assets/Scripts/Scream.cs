1a30410dd9edb193cbf97afe8a342bda37444893
Gregory Rose <greg@ncroses.com>
2014-11-18 13:15:55 -0500

    Add scream behavior

---
+++ Swarmlord/Assets/Scripts/Scream.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scream : MonoBehaviour {
+
+	public float MaxScale = 10.0f;
+	public float ScaleSpeed = 0.1f;
+
+	float scale;
+
+	// Use this for initialization
+	void Start () {
+		scale = 0.0f;
+		transform.localScale = new Vector3(scale,scale,scale);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		scale += ScaleSpeed;
+		transform.localScale = new Vector3(scale,scale,scale);
+		
+		if (scale > MaxScale)
+			Destroy(this);
+	}
+	
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.tag == "swarmer")
+			other.SendMessage("OnScream", transform.position);
+	}
+}
