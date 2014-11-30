83165b628449269a088c83bd36ff174faffe3e4f
Gregory Rose <greg@ncroses.com>
2014-11-30 15:07:18 -0500

    Add wander behavior

---
+++ Swarmlord/Assets/Scripts/Steering/Wander.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wander : Behavior {
+
+	private SwarmerController controller;
+
+	public Wander(SwarmerController controller) {
+		this.controller = controller;
+	}
+
+	private static float GetBinomialRandom() {
+		// Returns a random number between −1 and 1, where values around zero are more likely.
+		return Random.value - Random.value;
+	}
+
+	public override Steering GetSteering ()
+	{
+		Steering steering = new Steering();
+		
+		steering.angular = GetBinomialRandom() * controller.maxRotationSpeed;
+		steering.linear = controller.transform.up * controller.maxSpeed / 50;
+		
+		return steering;
+	}
+}
